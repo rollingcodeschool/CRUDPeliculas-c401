309bdc0d5a9342645edab4f6c82a6086b019a195
Emilse Arias <emilsearias@gmail.com>
2023-04-18 10:39:33 -0300

    agregamos el prototipo Pelicula y creamos un objeto nuevaPeli

---
+++ js/administrador.js
@@ -0,0 +1,5 @@
+import Pelicula from "./classPelicula.js";
+
+let nuevaPeli = new Pelicula(0, 'Super mario','alguna desc','-','url de imagen','aventura', 2023, 93,'EEUU','varios actores');
+
+console.log(nuevaPeli);
---
+++ js/classPelicula.js
@@ -0,0 +1,110 @@
+export default class Pelicula {
+    #codigo;
+    #titulo;
+    #descripcion;
+    #director;
+    #imagen;
+    #genero;
+    #anio;
+    #duracion;
+    #pais;
+    #reparto;
+  
+    constructor(codigo, titulo, descripcion, director, imagen, genero, anio, duracion, pais, reparto) {
+      this.#codigo = codigo;
+      this.#titulo = titulo;
+      this.#descripcion = descripcion;
+      this.#director = director;
+      this.#imagen = imagen;
+      this.#genero = genero;
+      this.#anio = anio;
+      this.#duracion = duracion;
+      this.#pais = pais;
+      this.#reparto = reparto;
+    }
+  
+    // Getters
+    get codigo() {
+      return this.#codigo;
+    }
+  
+    get titulo() {
+      return this.#titulo;
+    }
+  
+    get descripcion() {
+      return this.#descripcion;
+    }
+  
+    get director() {
+      return this.#director;
+    }
+  
+    get imagen() {
+      return this.#imagen;
+    }
+  
+    get genero() {
+      return this.#genero;
+    }
+  
+    get anio() {
+      return this.#anio;
+    }
+  
+    get duracion() {
+      return this.#duracion;
+    }
+  
+    get pais() {
+      return this.#pais;
+    }
+  
+    get reparto() {
+      return this.#reparto;
+    }
+  
+    // Setters
+    set codigo(codigo) {
+      this.#codigo = codigo;
+    }
+  
+    set titulo(titulo) {
+      this.#titulo = titulo;
+    }
+  
+    set descripcion(descripcion) {
+      this.#descripcion = descripcion;
+    }
+  
+    set director(director) {
+      this.#director = director;
+    }
+  
+    set imagen(imagen) {
+      this.#imagen = imagen;
+    }
+  
+    set genero(genero) {
+      this.#genero = genero;
+    }
+  
+    set anio(anio) {
+      this.#anio = anio;
+    }
+  
+    set duracion(duracion) {
+      this.#duracion = duracion;
+    }
+  
+    set pais(pais) {
+      this.#pais = pais;
+    }
+  
+    set reparto(reparto) {
+      this.#reparto = reparto;
+    }
+  }
+
+
+let prueba = 'esto es una prueba'
